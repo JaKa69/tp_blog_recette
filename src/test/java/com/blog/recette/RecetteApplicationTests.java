0186961b89654988a7f59c1a1351f80e71ae88d0
alix babin <alix.babin69@gmail.com>
2025-06-20 09:45:50 +0200

    init

---
+++ src/test/java/com/blog/recette/RecetteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.recette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecetteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
